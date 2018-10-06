fa47cda434d4985bf2a6a20779e02ec21dea6ca2
Biplab <biplab.n96@gmail.com>
2018-10-06 10:58:08 +0530

    Cosmetic changes in LiveData component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
 import logo from './logo.svg';
 import './App.css';
+import LiveData from './LiveData'
 
 class App extends Component {
   render() {
@@ -10,12 +12,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <LiveData />
+        </div>
       </div>
     );
   }
 }
+
 
 export default App;
--- src/LiveData.js
+++ src/LiveData.js
@@ -1,64 +1,42 @@
-import React from 'react';
-import { StyleSheet, ScrollView, Text, View, Dimensions } from 'react-native';
+import React, { Component } from 'react';
+import './styles/livedata.css';
 
-// const { width, height } = Dimensions.get('window');
-class LiveData extends React.Component {
+
+class LiveData extends Component{
+
     constructor(props) {
         super(props);
         this.state = {
-            currentPosition: 0,
-            stocks: [],
+            stocks: []
         };
-        this.scrolling = this.scrolling.bind(this);
+        // this.scrolling = this.scrolling.bind(this);
 
     }
 
-    // Activate scrolling animation
-    componentDidMount() {
-        this.activeInterval = setInterval(this.scrolling, 100);
-    }
-    // Get stock information from API
     componentWillMount() {
         this.getStockObj();
     }
-    // Symbols to be sent to API
+
     getSymbols() {
         let stockSymbols = [
-            // ...Commodity
+            'AAPL', 'GOOGL', 'GOOG', 'MSFT', 'FB',
+            'TSM', 'INTC', 'ORCL', 'CSCO', 'NVDA',
+            'IBM', 'SAP', 'TXN', 'QCOM', 'ADBE',
+            'AVGO', 'DCM', 'CRM', 'AABA', 'BIDU',
+            'ITW', 'ATVI', 'AMAT', 'ADP', 'MU',
+            'VMW', 'CTSH', 'INTU', 'NXPI', 'INFY',
+            'EA', 'ETN', 'HPQ', 'ADI', 'NOK',
+            'FISV', 'DXC', 'LRCX', 'NOW', 'HPE',
+            'WDC', 'WDAY', 'WIT', 'TWTR', 'ADSK',
+            'SNAP', 'WPP', 'RHT', 'KYO', 'CERN',
         ]
         return stockSymbols;
     }
-    // Scrolling Animation
-    scrolling() {
-        // Start scrolling if there's more than one stock to display
-        if (this.state.stocks.length > 1) {
-            // Increment position with each new interval
-            position = this.state.currentPosition + 5;
-            this.ticker.scrollTo(
-                { x: position, animated: true }
-            );
-            // After position passes this value, snaps back to beginning
-            let maxOffset = 20000;
-            // Set animation to repeat at end of scroll
-            if (this.state.currentPosition > maxOffset) {
-                this.ticker.scrollTo({ x: 0, animated: false })
-                this.setState({ currentPosition: 0 });
-            }
-            else {
-                this.setState({ currentPosition: position });
 
-            }
-        }
 
-    }
-    // Clear interval when user closes 
-    componentWillUnmount() {
-        clearInterval(this.activeInterval);
-    }
     async getStockObj() {
-
         let symbols = this.getSymbols();
-        let url = 'localhost:8080/getPrice?symbols=';
+        let url = 'https://api.iextrading.com/1.0/stock/market/batch?types=price,company&symbols=';
         for (let ctr = 0; ctr < symbols.length; ctr++) {
             url += symbols[ctr] + ',';
         }
@@ -78,70 +56,36 @@ class LiveData extends React.Component {
         }
     }
 
-    render() {
+
+
+    render(){
         if (this.state.stocks.length > 1) {
-            return (
-                <View>
-                    <ScrollView
-                        style={styles.scrollview}
-                        horizontal={true}
-                        ref={(ref) => this.ticker = ref}
-                        bounces={true}
-                    >
-                        {this.state.stocks.map((item, index) => (
-
-                            <View key={index} style={styles.view}>
-                                <View style={styles.hr} />
-
-                                <Text style={styles.text}>{item.company.companyName}</Text>
-                                <Text style={styles.text}>{item.company.symbol}</Text>
-                                <Text style={styles.text}>${item.price}</Text>
-                                <View style={styles.hr} />
-
-                            </View>
-                        ))}
-                    </ScrollView>
-
-                </View>
-
+            return(
+                <div className="ticker-wrap">
+                    <div className="ticker">
+                    {this.state.stocks.map((item, index) => (
+                        <div className="ticker__item"  key={index}>
+                            <p>{item.company.companyName}</p>
+                            <p>{item.company.symbol}</p>
+                            <p>${item.price}</p>
+                        </div>
+                    
+                ))}
+                </div>
+                </div>
             );
         }
+
         else {
             return (
-                <View style={styles.view}>
-                    <View style={styles.scrollview}>
-                        <Text style={styles.text}>Loading...</Text>
-                    </View>
-                </View>
+                <div>
+                    <div>
+                        <p>Loading...</p>
+                    </div>
+                </div>
             );
-        }
-
-    }
-
 }
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    text: {
-        paddingTop: 5,
-        paddingRight: width / 3,
-        color: 'black',
-        fontSize: 20,
-        fontWeight: 'bold'
-    },
-    scrollview: {
-        // height: height,
-        // width: width,
-        paddingLeft: width / 2,
-        backgroundColor: 'white',
+    }
+}
 
-    },
-    view: {
-        paddingTop: height / 2 - 75
-    },
-});
 export default LiveData;
